f0c11f2bc4bd8332d865845a345085e649d33dec
Afnan Latif <afnan.latif01@gmail.com>
2019-10-19 16:43:56 -0500

    Added stuff

--- main.py
+++ main.py
@@ -374,17 +374,18 @@ def trackRectsHuman(rects):
                     #scoreBoard total Missed update
                     totalMissedHuman = totalMissedHuman + 1
 
-#def fireAdjacent(shipHitsAI):
+def fireAdjacent(shipHitsAI):
     #shipHitAI is an array passed in with the current spot we want to hit
-    #for coord in reversed(shipHitAI):
-        #if(((coord[0]-1,coord[1]) not in rects_clickedAI) and (((coord[0]-1) >= 0) and ((coord[0]-1) <= 7)))
-            #return (coord[0]-1, coord[1])
-        #elif(((coord[0],coord[1]+1) not in rects_clickedAI) and (((coord[1]+1) >= 0) and ((coord[1]+1) <= 7)))
-            #return(coord[0], coord[1]+1)
-        #elif(((coord[0]+1,coord[1]) not in rects_clickedAI) and (((coord[0]+1) >= 0) and ((coord[0]+1) <= 7)))
-            #return (coord[0]+1, coord[1])
-        #elif(((coord[0],coord[1]-1) not in rects_clickedAI) and (((coord[1]-1) >= 0) and ((coord[1]-1) <= 7)))
-            #return(coord[0], coord[1]-1)
+    for coord in reversed(shipHitAI):
+        if(((coord[0]-1,coord[1]) not in rects_clickedAI) and (((coord[0]-1) >= 0) and ((coord[0]-1) <= 7)))
+            return (coord[0]-1, coord[1])
+        elif(((coord[0],coord[1]+1) not in rects_clickedAI) and (((coord[1]+1) >= 0) and ((coord[1]+1) <= 7)))
+            return(coord[0], coord[1]+1)
+        elif(((coord[0]+1,coord[1]) not in rects_clickedAI) and (((coord[0]+1) >= 0) and ((coord[0]+1) <= 7)))
+            return (coord[0]+1, coord[1])
+        elif(((coord[0],coord[1]-1) not in rects_clickedAI) and (((coord[1]-1) >= 0) and ((coord[1]-1) <= 7)))
+            return(coord[0], coord[1]-1)
+    #if none of these work, we need to go to the previous position in the for loop and try again, if there's no previous position, mediumAI needs to fire again randomly
     #if all of these are misses, medium needs to fire at another ranodm spot then call this again after the random shit is fired
 
 
